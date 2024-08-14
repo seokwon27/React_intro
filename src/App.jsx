77b8e3a02ebf0d5dfaf305195af601bafb55d3ad
unknown <tjrtkd95@gmail.com>
2024-08-14 10:46:08 +0900

    move: MedalTable 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import MedalTable from './components/MedalTable';
 
 function App() {
   //메달 현황
@@ -113,7 +114,7 @@ function App() {
       </form>
       <div>
         {/* 메달집계*/}
-        <Table medals={medals} deleteMedalHandler={deleteMedalHandler} />
+        <MedalTable medals={medals} deleteMedalHandler={deleteMedalHandler} />
       </div>
       <div style={{
         marginTop: "20px"
@@ -126,48 +127,6 @@ function App() {
 
 export default App
 
-//메달현황 컴포넌트
-const Table = ({ medals, deleteMedalHandler }) => {
-  return (
-    medals.length === 0 ? (
-      <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
-    ) : (
-      <table>
-        <thead key={"thead"}>
-          <tr>
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-        {
-          medals
-          .sort((a,b)=>{
-            return b.gold - a.gold || b.silver - a.silver || b.bronze - a.bronze
-          })
-          .map((medal) => {
-            const { country, gold, silver, bronze } = medal;
 
-            return (
-              <tbody key={country}>
-                <tr>
-                  <td>{country}</td>
-                  <td>{gold}</td>
-                  <td>{silver}</td>
-                  <td>{bronze}</td>
-                  <td>
-                    <button onClick={() => deleteMedalHandler(country)}>삭제</button>
-                  </td>
-                </tr>
-              </tbody>
-            )
-          })
-        }
 
-      </table>
-    )
-  )
-}
 
---
+++ src/components/MedalTable.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+//메달현황 컴포넌트
+const MedalTable = ({ medals, deleteMedalHandler }) => {
+    return (
+      medals.length === 0 ? (
+        <p>아직 추가된 국가가 없습니다. 메달을 추적하세요!</p>
+      ) : (
+        <table>
+          <thead key={"thead"}>
+            <tr>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>액션</th>
+            </tr>
+          </thead>
+          {
+            medals
+            .sort((a,b)=>{
+              return b.gold - a.gold || b.silver - a.silver || b.bronze - a.bronze
+            })
+            .map((medal) => {
+              const { country, gold, silver, bronze } = medal;
+  
+              return (
+                <tbody key={country}>
+                  <tr>
+                    <td>{country}</td>
+                    <td>{gold}</td>
+                    <td>{silver}</td>
+                    <td>{bronze}</td>
+                    <td>
+                      <button onClick={() => deleteMedalHandler(country)}>삭제</button>
+                    </td>
+                  </tr>
+                </tbody>
+              )
+            })
+          }
+  
+        </table>
+      )
+    )
+  }
+
+export default MedalTable
